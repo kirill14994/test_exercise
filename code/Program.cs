afe4cb056280b05f371cdfbdaba749872b24d23e
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2021-12-26 14:49:39 +0300

    add method PrintArray

--- code/Program.cs
+++ code/Program.cs
@@ -2,14 +2,14 @@
 // Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-int[] numbers = { 1, 8, 3, 10, 4, 9 };
+int[] numbers = { 1, 8, 3, 10, 4, 9, -2, 13, 5 };
 int[] evenNumbers = new int [numbers.Length];
 
 void PrintArray(int[] array)
 {
     for (int i = 0; i < numbers.Length; i++)
     {
-        Console.Write(array[i]);
+        if (array[i] != 0) Console.Write($"{array[i]} ");
     }
 }
 
@@ -17,3 +17,9 @@ for (int i = 0; i < numbers.Length; i++)
 {
     if (numbers[i] > 8) evenNumbers[i] = numbers[i];
 }
+
+Console.WriteLine("Даны числа: ");
+PrintArray(numbers);
+Console.WriteLine();
+Console.WriteLine("Числа больше 8: ");
+PrintArray(evenNumbers);
