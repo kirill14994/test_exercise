4080e177720d8b08391073bc2cccbc535f954891
kirill14994 <94280573+kirill14994@users.noreply.github.com>
2022-01-26 12:47:11 +0300

    edit PrintArray and SortArray

--- code/Program.cs
+++ code/Program.cs
@@ -7,24 +7,33 @@ void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] != 0) Console.Write($"{array[i]} ");
+        Console.Write($"{array[i]} ");
     }
 }
 
-void SortArray(int[] array, int[] newArray)
+int[] SortArray(int[] array)
 {
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i] > 8) newArray[i] = array[i];
-}
+    int index = 0;
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 8)
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+
+    }
+
+    Array.Resize(ref newArray, index);
+    return newArray;
 }
 
 int[] numbers = { 1, 8, 3, 10, 4, 9, -2, 13, 5 };
-int[] evenNumbers = new int [numbers.Length];
+
 
 Console.WriteLine("Даны числа: ");
 PrintArray(numbers);
 Console.WriteLine();
 Console.WriteLine("Числа больше 8: ");
-SortArray(numbers, evenNumbers);
-PrintArray(evenNumbers);
+PrintArray(SortArray(numbers));
